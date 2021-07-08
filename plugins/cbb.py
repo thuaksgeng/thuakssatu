7040b804b994154d2402b02889f1a128bdf7682c
thuaksgeng <85960110+thuaksgeng@users.noreply.github.com>
2021-07-08 11:03:35 +0700

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,28 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ MY LORD : <a href='tg://user?id={OWNER_ID}'>GUE</a>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Oke", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
---
+++ plugins/link_generator.py
@@ -0,0 +1,60 @@
+#(©)Codexbotz
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from bot import Bot
+from config import ADMINS
+from helper_func import encode, get_message_id
+
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+async def batch(client: Client, message: Message):
+    while True:
+        try:
+            first_message = await client.ask(text = "Forward file pertama yg pengen lu upload (jangan lupa pake caption atau keterangan)..\n\natau lu bisa kirimin gue link filenya dari database channel", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
+        except:
+            return
+        f_msg_id = await get_message_id(client, first_message)
+        if f_msg_id:
+            break
+        else:
+            await first_message.reply("❌ Error GOBLOK kirim link atau file yang bener napa\n\nGue nerima yang dari database doang\nSelain itu ERROR GOBLOK", quote = True)
+            continue
+
+    while True:
+        try:
+            second_message = await client.ask(text = "Forward file terakhir yg pengen lu upload (jangan lupa pake caption atau keterangan)..\n\natau lu bisa kirimin gue link filenya dari database channel", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
+        except:
+            return
+        s_msg_id = await get_message_id(client, second_message)
+        if s_msg_id:
+            break
+        else:
+            await second_message.reply("❌ Error GOBLOK kirim link atau file yang bener napa\n\nGue nerima yang dari database doang\nSelain itu ERROR GOBLOK", quote = True)
+            continue
+
+
+    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Share Link", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>Ini linknya tuan</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+
+
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+async def link_generator(client: Client, message: Message):
+    while True:
+        try:
+            channel_message = await client.ask(text = "Forward file yg pengen lu upload (jangan lupa pake caption atau keterangan)..\n\natau lu bisa kirimin gue link filenya dari database channel", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=30)
+        except:
+            return
+        msg_id = await get_message_id(client, channel_message)
+        if msg_id:
+            break
+        else:
+            await channel_message.reply("❌ Error GOBLOK kirim link atau file yang bener napa\n\nGue nerima yang dari database doang\nSelain itu ERROR GOBLOK", quote = True)
+            continue
+
+    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Share Link", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>Ini linknya tuan</b>\n\n{link}", quote=True, reply_markup=reply_markup)
